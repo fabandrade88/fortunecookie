ced60674154263553d0175ebadb0081d771ab2f2
Fabricio Andrade <fabricio.solutions13@gmail.com>
2024-05-18 02:25:22 +0200

    final js

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 let clickCookie = document.querySelector(".btn-click")
 let screen1 = document.querySelector(".screen1")
 let screen2 = document.querySelector(".screen2")
+let btnReturn = document.querySelector(".btn01")
+let h2Text = document.querySelector(".h222");
 
-clickCookie.addEventListener('click', toggleScreen)
+
+let randomText = [
+  "You have to love your husband",
+  "Your husband is the best",
+  "You will smell your husband farts",
+  "YOu need to love him forever"
+];
+
+clickCookie.addEventListener('click', function() {
+  toggleScreen();
+  updateRandomText();
+});
+
+btnReturn.addEventListener('click', toggleScreen)
 
 function toggleScreen() {
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
+}
+
+function randomIndex() {
+  return Math.floor(Math.random() * randomText.length);
+}
+
+function updateRandomText() {
+  let index = randomIndex();
+  h2Text.innerText = randomText[index];
 }
