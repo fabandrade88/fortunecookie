7ea817a1cd8c369f66af496b17313cb702a4733a
Fabricio Andrade <fabricio.solutions13@gmail.com>
2024-05-18 02:40:59 +0200

    Description

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let screen2 = document.querySelector(".screen2")
 let btnReturn = document.querySelector(".btn01")
 let h2Text = document.querySelector(".h222");
 
+//texto aleatorios
 
 let randomText = [
   "You have to love your husband",
@@ -12,21 +13,31 @@ let randomText = [
   "You need to love him forever"
 ];
 
+//evento sobre a imagem do cookie 
+
 clickCookie.addEventListener('click', function() {
   toggleScreen();
   updateRandomText();
 });
 
+//evento de retorno para inicio
+
 btnReturn.addEventListener('click', toggleScreen)
+
+//funcao de esconder as telas
 
 function toggleScreen() {
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
 }
 
+//funcao de criar uma selecao aleatorio dos textos na variavel
+
 function randomIndex() {
   return Math.floor(Math.random() * randomText.length);
 }
+
+//inserir textos aleatorios na variavel H2
 
 function updateRandomText() {
   let index = randomIndex();
